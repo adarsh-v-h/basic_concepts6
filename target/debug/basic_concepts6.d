d0e0972d1621617f4f4dd85bc58a79a3da4bc67e
adarsh-v-h <adarshvh2005@gmail.com>
2025-09-04 16:01:48 +0530

    Lets go

---
+++ target/debug/basic_concepts6.d
@@ -0,0 +1,1 @@
+C:\Users\adars\OneDrive\Documents\vs\ code\RUST\basic_concepts6\target\debug\basic_concepts6.exe: C:\Users\adars\OneDrive\Documents\vs\ code\RUST\basic_concepts6\src\main.rs
---
+++ target/debug/deps/basic_concepts6.d
@@ -0,0 +1,5 @@
+C:\Users\adars\OneDrive\Documents\vs code\RUST\basic_concepts6\target\debug\deps\basic_concepts6.d: src\main.rs
+
+C:\Users\adars\OneDrive\Documents\vs code\RUST\basic_concepts6\target\debug\deps\basic_concepts6.exe: src\main.rs
+
+src\main.rs:
